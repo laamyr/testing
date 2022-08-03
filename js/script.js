16b89b0c4a2686f8539f9e8b31fa13368f1c07b1
laamyr <110023218+laamyr@users.noreply.github.com>
2022-08-03 01:49:58 +0100

    Add files via upload

---
+++ js/script.js
@@ -0,0 +1,8 @@
+/********** typing animation***********/
+/*
+var typed = new Typed(".typing",{
+    Strings:["Web Developer","Web Design","Graphic Designer"],
+    typeSpeed:100,
+    backSpeed:60,
+    loop:true
+})*/
